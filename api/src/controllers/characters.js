15a336583c1a54f91abe4fff4905cc110bb99ac1
Mauricio776 <mauuuricio@hotmail.com>
2023-04-12 00:17:06 -0300

    fixed favorites front connect new server and store redux

--- api/src/controllers/characters.js
+++ api/src/controllers/characters.js
@@ -1,62 +1,68 @@
 const axios = require("axios");
 
-function getCharacterId(req, res, id) {
-  axios
-    .get(`https://rickandmortyapi.com/api/character/${id}`)
-    .then((v) => v.data)
-    .then((ch) => {
-      console.log(ch);
-      const character = {
-        id: ch.id,
-        name: ch.name,
-        gender: ch.gender,
-        species: ch.species,
-        origin: ch.origin?.name,
-        image: ch.image,
-        status: ch.status,
-      };
-      res
-        .writeHead(200, { "Content-type": "application/json" })
-        .end(JSON.stringify(character));
+require("dotenv").config(); // process.env
+
+const URL = process.env.API_URL;
+const EMAIL = process.env.EMAIL;
+const PASSWORD = process.env.PASSWORD;
+
+const STATUS_OK = 200;
+const STATUS_ERROR = 404;
+
+function getCharById(req, res) {
+  const { id } = req.params;
+  try {
+    axios.get(`${URL}${id}`).then(({ data }) => {
+      if (data) {
+        const character = {
+          id: data.id,
+          status: data.status,
+          name: data.name,
+          species: data.species,
+          origin: data.origin?.name,
+          image: data.image,
+          gender: data.gender,
+          location: data.location?.name
+        };
+        res.status(STATUS_OK).json(character);
+      } else {
+        res.status(STATUS_ERROR).json({ message: "character not found" });
+      }
     });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 }
 
-function getDetailId(req, res, id) {
-  axios
-    .get(`https://rickandmortyapi.com/api/character/${id}`)
-    .then((v) => v.data)
-    .then((ch) => {
-      console.log(ch);
-      const character = {
-        id: ch.id,
-        name: ch.name,
-        gender: ch.gender,
-        species: ch.species,
-        origin: ch.origin?.name,
-        image: ch.image,
-        status: ch.status,
-        location: ch.location.name,
-      };
-      res
-        .writeHead(200, { "Content-type": "application/json" })
-        .end(JSON.stringify(character));
+function getAllChar(req, res) {
+  try {
+    axios.get(`${URL}`).then(({ data }) => {
+      if (data) {
+        // *** ERROR al usar filter y no map para editar los campos y que estaba en data.results
+        const characters = data.results.map((ch) => {
+          const character = {
+            id: ch.id,
+            status: ch.status,
+            name: ch.name,
+            species: ch.species,
+            origin: ch.origin?.name,
+            image: ch.image,
+            gender: ch.gender,
+          };
+          return character;
+        });
+        // console.log(":::", characters);
+        res.status(STATUS_OK).json(characters);
+      } else {
+        res.status(STATUS_ERROR).json({ message: "character not found" });
+      }
     });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 }
 
-/*
- <img src={character.image} alt={character.name} />
-      <div>
-        <h1>{character.name}</h1>
-        <h2>{character.species}</h2>
-      </div>
-
-      <div className={style.text_contain}>
-        <h3>{character.status}</h3>
-        <h3>{character.gender}</h3>
-        <h3>{character.origin?.name}</h3>
-        <h3>{character.location?.name}</h3>
-*/
 module.exports = {
-  getCharacterId,
-  getDetailId,
+  getCharById,
+  getAllChar,
 };
